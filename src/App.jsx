417b09accdb8c85ed0f4fc9a9a081a4356f44571
shreeyansh-29 <shreeyansh.rawat@gmail.com>
2024-03-04 14:07:45 +0530

    react-query

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import PostLists from "./components/postLists";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2 className="title">My Posts</h2>
+      <PostLists />
+    </div>
+  );
+}
+
+export default App;
